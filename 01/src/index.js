4ad92007d6b62afd009d7e29dd3eb3153636c2d9
CoolePizza <hello@coolepizza.de>
2022-06-09 11:24:48 +0200

    update 01 to v14

--- 01/src/index.js
+++ 01/src/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config() //Dotenv Importieren und die config() Methode ausführen um die .env Datei zu laden.
-const { Client } = require("discord.js") //Von Discord.js Client importieren.
+const { Client, ActivityType } = require("discord.js") //Von Discord.js Client importieren.
 
 const client = new Client({intents:[]}) //Den Client inizialisieren, ohne Intents (Intents Link).
 
@@ -7,7 +7,7 @@ client.once("ready", () => { //Einmalig das Ready Event abfangen (Wenn der Bot s
     //Nachricht, wenn der Bot sich eingeloggt hat ausgeben, die den Tag des Bottes (Name und Discriminator) 
     //und Anzahl der Guilds, wo der Bot drauf ist enthällt.
     console.log(`Ready! Logged in as ${client.user.tag}! I'm on ${client.guilds.cache.size} guild(s)!`)
-    client.user.setActivity({name: "mit dem Code", type: "PLAYING"}) //Activity beim starten des Bottes anzeigen.
+    client.user.setActivity({name: "mit dem Code", type: ActivityType.Playing}) //Activity beim starten des Bottes anzeigen.
 })
 
 client.login(process.env.DISCORD_BOT_TOKEN) //Client mit dem Token aus der .env Datei einloggen lassen!
